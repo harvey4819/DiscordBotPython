37c484734bc77fadbea4de58a8c0799f9bb6be14
Harvey <117118100+harvey4819@users.noreply.github.com>
2023-01-02 15:44:19 +0000

    Reverted back to one command due to issues with the other commands not working

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ client = discord.Client(intents=discord.Intents.all())
 @client.event
 async def on_ready():
   print('We have lift off {0.user} yay!'.format(client))
+  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="bleep bloop bop"))
 
 @client.event
 async def on_message(message):
@@ -14,23 +15,5 @@ async def on_message(message):
 
   if message.content.startswith('/hello'):
     await message.channel.send('Hello!')
-
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-    
-  if message.content.startswith('/ping'):
-    await message.channel.send('Pong!')
-
-@client.event
-async def on_message(message):
-  if message.author == client.user:
-    return
-
-  if message.content.startswith("/hru"):
-    await message.channel.send('I am good thank you!')
-
-
 
 client.run(os.environ['TOKEN'])
