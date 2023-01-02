2d8a001b7b14b93fc9d41ef0c585c5b0662e6ee1
harvey481999 <>
2023-01-02 12:02:02 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+import os
+
+client = discord.Client(intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+  print('We have lift off {0.user} yay!'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('/hello'):
+    await message.channel.send('Hello!')
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+    
+  if message.content.startswith('/ping'):
+    await message.channel.send('Pong!')
+
+client.run(os.environ['TOKEN'])
