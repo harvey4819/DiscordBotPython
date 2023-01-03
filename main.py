72650a9eccdf7db2a3bfb0eade2871b590c04578
Harvey <117118100+harvey4819@users.noreply.github.com>
2023-01-03 17:35:12 +0000

    Added a coin flip command

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Packages
 import discord
 import os
+import random
 from discord.ext import commands
 
 #Intents
@@ -10,11 +11,19 @@ client = commands.Bot(command_prefix = "/", intents = discord.Intents.all())
 @client.event
 async def on_ready():
   print('{0.user} We have lift off!'.format(client))
-  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='bleep bloop bop'))
+  await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='Robot Pop'))
 
-#Commands
-@client.command()
+#COMMANDS START
+#Hi command
+@client.command(name="hi", description="Bot will reply with hello whilst mentioning the author of the message")
 async def hi(ctx):
   await ctx.send('Hello, ' + ctx.message.author.mention)
+
+#Coin flip command
+@client.command(name='cflip', description="Bot will flip a coin and output the result")
+async def cflip(ctx):
+  response = ['heads', 'tails']
+  await ctx.send(ctx.message.author.mention + f' you got {random.choice(response)}!')
+#COMMANDS END
 
 client.run(os.environ['TOKEN1'])
